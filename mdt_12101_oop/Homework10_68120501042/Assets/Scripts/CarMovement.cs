0005163c90ce2e91576d720c321c199061e262ea
Sirisart <alternativelegobrick@gmail.com>
2026-05-22 15:14:05 +0700

    feat: complete all requirement for mdt_12101/Homework10

---
+++ mdt_12101_oop/Homework10_68120501042/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    transform.position = new UnityEngine.Vector3((transform.position.x + (5f * Time.deltaTime) * -1), 0.55f, transform.position.z);
+  }
+}
